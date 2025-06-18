b6c718c9d297144ecb16ff5b100b840432eae05e
yanghyeran1215@gmail.com <yanghyeran1215@gmail.com>
2025-06-18 11:11:54 +0900

    03 추가

---
+++ 03/modules/ch03_01.js
@@ -0,0 +1,4 @@
+const math = require("./modules/math");
+
+console.log(`math.add(5,3)=>${math.add(5, 3)}`);
+console.log(`math.subtract(10,4)=>${math.subtract(10, 4)}`);
---
+++ 03/modules/math.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  //외부로 내보내기
+  add,
+  subtract,
+};
